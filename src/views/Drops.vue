f688dce13dd790c9e0aaab1742a7380fc5259918
shayon <sha7onpro@gmail.com>
2021-11-08 15:56:20 +0600

    Updated Zero2One

--- src/views/Drops.vue
+++ src/views/Drops.vue
@@ -50,7 +50,7 @@ components: {
         { id:1, name:"Animal Farm by George Orwell", LastLoc: "Gloria Jean's Coffee, Dhanmondi", LastDropped: "2021-10-14", PickUp: "Not yet"},
        // { id:1, name:"Schoolgirl by Osamu Dazai", LastLoc: "North End Coffee Roasters, Dhanmondi", LastDropped: "2021-11-01", PickUp: "Not yet"},
         //{ id:1, name:"The Archer by Paulo Coelho", LastLoc: "North End Coffee Roasters, Dhanmondi", LastDropped: "2021-11-15", PickUp: "Not yet"},
-        //{ id:1, name:"Zero to One by Peter Thielf", LastLoc: "North End Coffee Roasters, Dhanmondi", LastDropped: "2021-12-01", PickUp: "Not yet"},
+        { id:1, name:"Zero to One by Peter Thielf", LastLoc: "Crimson Cup, Dhanmondi", LastDropped: "2021-11-04", PickUp: "Not yet"},
       
     
       ],
