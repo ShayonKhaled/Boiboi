8aae590af4781b485f59b04d61460e43cf5bfb35
shayon <sha7onpro@gmail.com>
2021-12-02 15:50:35 +0600

    The Archer drop

--- src/views/Drops.vue
+++ src/views/Drops.vue
@@ -49,7 +49,7 @@ components: {
       rows: [
         { id:1, name:"Animal Farm by George Orwell", LastLoc: "Gloria Jean's Coffee, Dhanmondi", LastDropped: "2021-10-14", PickUp: "Not yet"},
        // { id:1, name:"Schoolgirl by Osamu Dazai", LastLoc: "North End Coffee Roasters, Dhanmondi", LastDropped: "2021-11-01", PickUp: "Not yet"},
-        //{ id:1, name:"The Archer by Paulo Coelho", LastLoc: "North End Coffee Roasters, Dhanmondi", LastDropped: "2021-11-15", PickUp: "Not yet"},
+        { id:1, name:"The Archer by Paulo Coelho", LastLoc: "Crimson Cup, Dhanmondi", LastDropped: "2021-12-2", PickUp: "Not yet"},
         { id:1, name:"Zero to One by Peter Thielf", LastLoc: "Crimson Cup, Dhanmondi", LastDropped: "2021-11-04", PickUp: "Not yet"},
       
     
